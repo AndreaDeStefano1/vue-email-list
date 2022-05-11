19128986dbcb45f74888a05158afb44feadec0a7
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-05-11 15:09:53 +0200

    Il programma chiama 10 volte l API e stampa 10 mail

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+
+console.log('ciao')
+
+const app = new Vue({
+  el: '#app',
+
+  data:{
+    mailList: [],
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((res) => {
+       
+        this.mailList.push(res.data.response);
+      })
+      
+      
+    }
+
+  }
+ 
+})
+
